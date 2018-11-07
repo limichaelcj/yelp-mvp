e31b033054288f5acd1971ac4fa03b198ba1790d
Michael Li <mike830@gmail.com>
2018-11-07 12:02:18 +0900

    add models and controllers

---
+++ app/models/review.rb
@@ -0,0 +1,7 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+
+  validates :restaurant, :content, :rating, presence: true
+  validates :rating, numericality: { only_integer: true },
+                     inclusion: { in: (0..5).to_a }
+end
